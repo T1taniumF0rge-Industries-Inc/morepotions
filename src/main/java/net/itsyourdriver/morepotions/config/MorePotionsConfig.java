a1b554f3553de4c1d6cee806e550b9e001e1ec11
Titan1um <gamersoftware.corp@gmail.com>
2026-02-27 15:59:00 +0100

    Add potion extension/amplification progression rules

---
+++ src/main/java/net/itsyourdriver/morepotions/config/MorePotionsConfig.java
@@ -0,0 +1,60 @@
+package net.itsyourdriver.morepotions.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MorePotionsConfig {
+    public static boolean LEVITATION_POTION_ENABLED = true;
+    public static boolean DECAY_POTION_ENABLED = true;
+    public static boolean NAUSEA_POTION_ENABLED = true;
+    public static boolean LUCK_POTION_ENABLED = true;
+    public static boolean GLOWING_POTION_ENABLED = true;
+    public static boolean BLINDNESS_POTION_ENABLED = true;
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private static class Data {
+        boolean LEVITATION_POTION_ENABLED = true;
+        boolean DECAY_POTION_ENABLED = true;
+        boolean NAUSEA_POTION_ENABLED = true;
+        boolean LUCK_POTION_ENABLED = true;
+        boolean GLOWING_POTION_ENABLED = true;
+        boolean BLINDNESS_POTION_ENABLED = true;
+    }
+
+    public static void load() {
+        Path path = FabricLoader.getInstance().getConfigDir().resolve("morepotions-common.json");
+
+        Data data = new Data();
+        try {
+            if (Files.notExists(path)) {
+                saveDefaults(path);
+            }
+
+            String configJson = Files.readString(path);
+            Data parsedData = GSON.fromJson(configJson, Data.class);
+            if (parsedData != null) {
+                data = parsedData;
+            }
+        } catch (IOException | JsonParseException ignored) {
+        }
+
+        LEVITATION_POTION_ENABLED = data.LEVITATION_POTION_ENABLED;
+        DECAY_POTION_ENABLED = data.DECAY_POTION_ENABLED;
+        NAUSEA_POTION_ENABLED = data.NAUSEA_POTION_ENABLED;
+        LUCK_POTION_ENABLED = data.LUCK_POTION_ENABLED;
+        GLOWING_POTION_ENABLED = data.GLOWING_POTION_ENABLED;
+        BLINDNESS_POTION_ENABLED = data.BLINDNESS_POTION_ENABLED;
+    }
+
+    private static void saveDefaults(Path path) throws IOException {
+        Files.createDirectories(path.getParent());
+        Files.writeString(path, GSON.toJson(new Data()));
+    }
+}
--- src/main/java/net/itsyourdriver/morepotions/potion/ModPotions.java
+++ src/main/java/net/itsyourdriver/morepotions/potion/ModPotions.java
@@ -1,45 +1,86 @@
 package net.itsyourdriver.morepotions.potion;
 
 import net.itsyourdriver.morepotions.MorePotions;
-import net.itsyourdriver.morepotions.config.MorePotionsCommonConfigs;
-import net.itsyourdriver.morepotions.util.BetterBrewingRecipe;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.Potions;
-import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
-public class ModPotions {
-    public static final DeferredRegister<Potion> POTIONS
-            = DeferredRegister.create(ForgeRegistries.POTIONS, MorePotions.MOD_ID);
+public final class ModPotions {
+    public static final Potion LEVITATION_POTION = registerPotion("levitation_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.LEVITATION, 600, 0)));
+    public static final Potion LONG_LEVITATION_POTION = registerPotion("long_levitation_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.LEVITATION, 1800, 0)));
+    public static final Potion STRONG_LEVITATION_POTION = registerPotion("strong_levitation_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.LEVITATION, 600, 1)));
 
-    // Levitation
-    public static final RegistryObject<Potion> LEVITATION_POTION = POTIONS.register("levitation_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.LEVITATION, 600, 0)));
+    public static final Potion DECAY_POTION = registerPotion("decay_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.WITHER, 900, 0)));
+    public static final Potion LONG_DECAY_POTION = registerPotion("long_decay_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.WITHER, 1800, 0)));
+    public static final Potion STRONG_DECAY_POTION = registerPotion("strong_decay_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.WITHER, 900, 1)));
 
-    // Withering
-    public static final RegistryObject<Potion> DECAY_POTION = POTIONS.register("decay_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.WITHER, 900, 0)));
+    public static final Potion NAUSEA_POTION = registerPotion("nausea_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.NAUSEA, 300, 0)));
+    public static final Potion LONG_NAUSEA_POTION = registerPotion("long_nausea_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.NAUSEA, 900, 0)));
+    public static final Potion STRONG_NAUSEA_POTION = registerPotion("strong_nausea_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.NAUSEA, 300, 1)));
 
-    // Nausea Potion
-    public static final RegistryObject<Potion> NAUSEA_POTION = POTIONS.register("nausea_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.CONFUSION, 600, 0))); // IDK why nausea is named confusion but oh well
+    public static final Potion GLOWING_POTION = registerPotion("glowing_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.GLOWING, 3600, 0)));
+    public static final Potion LONG_GLOWING_POTION = registerPotion("long_glowing_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.GLOWING, 9600, 0)));
 
-    // Glowing Potion
-    public static final RegistryObject<Potion> GLOWING_POTION = POTIONS.register("glowing_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.GLOWING, 3600, 0))); // me when the glow berry doesnt make me glow (i got scammed)
+    public static final Potion BLINDNESS_POTION = registerPotion("blindness_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.BLINDNESS, 300, 0)));
+    public static final Potion LONG_BLINDNESS_POTION = registerPotion("long_blindness_potion",
+            new Potion(new StatusEffectInstance(StatusEffects.BLINDNESS, 900, 0)));
 
-    // Blindness Potion
-    public static final RegistryObject<Potion> BLINDNESS_POTION = POTIONS.register("blindness_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 300, 0)));
+    private static final int[] LUCK_DURATIONS = {6000, 18000, 36000, 72000};
+    private static final Potion[][] LUCK_VARIANTS = createLuckVariants();
 
-    public static void register(IEventBus eventBus) {
-        POTIONS.register(eventBus);
+    private ModPotions() {
     }
 
+    private static Potion[][] createLuckVariants() {
+        Potion[][] variants = new Potion[5][4];
+        variants[0][0] = Potions.LUCK;
+
+        for (int levelIndex = 0; levelIndex < variants.length; levelIndex++) {
+            for (int extensionIndex = 0; extensionIndex < variants[levelIndex].length; extensionIndex++) {
+                if (levelIndex == 0 && extensionIndex == 0) {
+                    continue;
+                }
+
+                String id = "luck_l" + (levelIndex + 1) + "_e" + extensionIndex;
+                variants[levelIndex][extensionIndex] = registerPotion(id,
+                        new Potion(new StatusEffectInstance(StatusEffects.LUCK, LUCK_DURATIONS[extensionIndex], levelIndex)));
+            }
+        }
+
+        return variants;
+    }
+
+    public static Potion getLuckPotion(int level, int extensionTier) {
+        if (level < 1 || level > 5) {
+            throw new IllegalArgumentException("Luck level must be in [1..5]");
+        }
+        if (extensionTier < 0 || extensionTier > 3) {
+            throw new IllegalArgumentException("Luck extension tier must be in [0..3]");
+        }
+
+        return LUCK_VARIANTS[level - 1][extensionTier];
+    }
+
+    private static Potion registerPotion(String id, Potion potion) {
+        return Registry.register(Registries.POTION, new Identifier(MorePotions.MOD_ID, id), potion);
+    }
+
+    public static void init() {
+    }
 }
